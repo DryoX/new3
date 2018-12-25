918f6d8961ab42e3a0f1204389f184e1db317095
DryoX <40540721+DryoX@users.noreply.github.com>
2018-12-25 11:47:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,41 @@
-const Discord = require("discord.js");
-const botconfig = require("./botconfig.json");
-const bot = new Discord.Client({
-	disableEveryone: true
-});
+;
+    const Discord = require("discord.js");
+const client = new Discord.Client({disableEveryone: true});
+const check = [
+   "Hello!",
+   "Created by Next",
+   "Version 1.0.0"
+];
 
-const asy = ["Halo!", "Merry Christmas 25/18!", "Happy New Year 18/19!"];
-
-//Bot will start from here!
-
-bot.on("ready", async () => {
-	console.log(`Hello i'm ${bot.user.username} ready to help!`)
+client.on("Ready", async () => {
+	console.log(`i'm ${client.user.username} ready to help!`)
 	 setInterval(() => {
- 		let crq = Math.floor(Math.random() * (asy.length - 1) + 1)
- 		bot.user.setActivity(asy[crq])
- 	}, 4000)
-});
+	    let cof = Math.floor(Math.random () * (check.length + 1) - 1);
+	    client.user.setActivity(check[cof])
+	}, 4000);
+	
+	});
 
-bot.on("message", async message => {
+client.on("message", async message => {
 	if(message.author.bot) return;
 	if(message.channel.type === "dm") return;
+
+
+let messageArray = message.content.split(" ");
+let args = messageArray.slice(1);
+let cmd = messageArray[0];
+let prefix = "xa!";
+
+if(cmd === `{prefix}say`){
 	
-	let prefix = botconfig.prefix;
-	let messageArray = message.content.split(" ");
-	let cmd = messageArray[0];
-	let args = messageArray.slice(1);
-	
-        if(cmd === `${prefix}info`){
-		
-		let wirav = new Discord.RichEmbed()
-		.setColor("RANDOM")
-		.addField("Information", "Created By Next");
-		
-		return message.channel.send(wirav);
-		
-	}
-	
-	if(cmd === `${prefix}say`){
-		
-		let says = args.join(" ");
-		if(!says) return message.channel.send("Do i need to say something?").then(message => {message.delete(5000)});
-	
-	    return message.channel.send(says);
-	
-	}
-	
-	if(cmd === `${prefix}bug`){
-		
-		let rep = args.join(" ");
-		if(!rep) return message.channel.send("Any report?");
-		
-		let afg = new Discord.RichEmbed()
-		   .setColor("RANDOM")
-		   .addField("Bug report!", rep);
-		   
-		   bot.users.get("378074425066520577").send(afg);	
-		
-           }
+let say = args.join(" ");
+if(!say) return message.channel.send("Anything to say?");
+
+message.catch.delete
+return message.channel.send(say);
+
+   }
 
 });
 
-
-bot.login(process.env.BOT_TOKEN);
-    
+client.login(process.env.BOT_TOKEN);
